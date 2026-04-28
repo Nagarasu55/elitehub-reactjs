c6ae076b5f3c94f71e053de769068f747002d761
Nagarasu veerappan <nagarasu@elitecorpusa.com>
2026-04-28 16:58:16 +0530

    delete unused code

--- src/service/socket.ts
+++ src/service/socket.ts
@@ -4,7 +4,7 @@ let socket: Socket;
 
 export const getSocket = (): Socket => {
     if (!socket) {
-        socket = io("http://localhost:5000");
+        socket = io(import.meta.env.VITE_API_BASE_URL);
     }
     return socket;
 };
